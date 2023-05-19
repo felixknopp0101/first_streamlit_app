0eb8567863f80d18c518c1acb4cf168786caf403
felixknopp0101 <133718302+felixknopp0101@users.noreply.github.com>
2023-05-19 17:58:30 +0200

    Update streamlit_app.py
    added fruit_to_show dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,16 @@ import pandas
 
 streamlit.title('My Parents new healthy diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('eggs and spinach')
+streamlit.text('Create your own smoothie!')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+selected_fruits = streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = selected_fruits.loc[fruits_selected]
 
 streamlit.dataframe(my_fruit_list)
+st.dataframe(fruits_to_show)
                   
