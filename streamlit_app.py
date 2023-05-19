64ad533faf7842a2e6326047c1bea1498088cedb
felixknopp0101 <133718302+felixknopp0101@users.noreply.github.com>
2023-05-19 17:44:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('eggs and spinach')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.index('Fruit')
 
 
 streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index))
