39a12293f24ddf72739ebe4cc991e777fb832553
felixknopp0101 <133718302+felixknopp0101@users.noreply.github.com>
2023-05-19 17:33:52 +0200

    Update streamlit_app.py
    added panda, created dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit
+import pandas
+
 streamlit.title('My Parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('eggs and spinach')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
                   
